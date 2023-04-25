23dad6263b7b1480cf2ea5b6ac8ee5bbfb4ce97a
Rita <shaidrv@gmail.com>
2023-04-25 19:53:46 +0300

    Переключение шагов формы, анимация перехода между шагами
    Вывод количества шагов в шаблон. 
    Автоматический переход  на следующую страницу при выборе radio.
    ajax заглушка при отправке формы. 
    неактивная кнопка "далее" пока ничего не выбрано

--- main.js
+++ main.js
@@ -1,2 +1,59 @@
 import 'normalize.css'
 import './style.css'
+
+addEventListener('load', () => {
+  const forms = document.querySelectorAll('.form')
+
+  document.querySelectorAll('.form__button_next').forEach((button, idx) => {
+    button.addEventListener('click', () => {
+      const nextFormIndex = idx + 1
+      const currentFormIndex = idx
+      forms[nextFormIndex].classList.remove('hidden')
+      forms[currentFormIndex].classList.add('hidden')
+    })
+  })
+
+  document.querySelectorAll('.form__button_back').forEach((button, idx) => {
+    button.addEventListener('click', () => {
+      const prevFormIndex = idx
+      const currentFormIndex = idx + 1
+      forms[prevFormIndex].classList.remove('hidden')
+      forms[currentFormIndex].classList.add('hidden')
+    })
+  })
+
+  document.querySelectorAll('.forms__count').forEach((span) => {
+    span.innerHTML = forms.length
+  })
+
+  forms.forEach((form) => {
+    const btn = form.querySelector('.form__button_next')
+    form.querySelectorAll('input[type=radio], select').forEach((input) => {
+      input.addEventListener('change', () => {
+        btn.removeAttribute('disabled')
+        if (input.type === 'radio') {
+          btn.click()
+        }
+      })
+    })
+  })
+
+  document
+    .querySelector('.button__large')
+    .addEventListener('click', async () => {
+      const data = {}
+      document
+        .querySelectorAll('input[type=text], input[type=radio]:checked, select')
+        .forEach((input) => {
+          data[input.name] = input.value
+        })
+      const response = await fetch('https://dummyjson.com/posts/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      alert('Успешно отправлено!')
+    })
+})
